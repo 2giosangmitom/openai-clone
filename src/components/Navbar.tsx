57ea72d6fe8f43f35c63e824d2fe855cf8b5f804
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-28 15:32:53 +0700

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,24 +8,24 @@ const Navbar: React.FC = () => {
         <li className='mr-12'>
           <Image className='cursor-pointer w-auto h-4' src={logo} alt='logo' />
         </li>
-        <li className='p-3'>
+        <li className='p-3 hover:text-sky-500 duration-300'>
           <a href=''>Home</a>
         </li>
-        <li className='p-3'>
+        <li className='p-3 hover:text-sky-500 duration-300'>
           <a href=''>What is GPT ?</a>
         </li>
-        <li className='p-3'>
+        <li className='p-3 hover:text-sky-500 duration-300'>
           <a href=''>Open AI</a>
         </li>
-        <li className='p-3'>
+        <li className='p-3 hover:text-sky-500 duration-300'>
           <a href=''>Case Studies</a>
         </li>
-        <li className='p-3'>
+        <li className='p-3 hover:text-sky-500 duration-300'>
           <a href=''>Library</a>
         </li>
       </ul>
       <div>
-        <span className='p-2 cursor-pointer'>Sign in</span>
+        <span className='p-2 cursor-pointer hover:text-sky-500 duration-300'>Sign in</span>
         <span className='p-2 cursor-pointer bg-orange-600 rounded-md duration-200 hover:bg-orange-700 z-10'>
           Sign up
         </span>
