c806d71a25ecab985732ca947b98ca1e013fb1a5
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2023-06-28 19:35:11 +0700

    add new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
       dropShadow: {
         image: "0 0 0.5em #4ac7d1",
       },
+      keyframes: {
+        scale: {
+          "0%, 100%": {
+            transform: "scale(1)",
+          },
+          "50%": {
+            transform: "scale(0.95)",
+            filter: "drop-shadow(0 0 1em #4ac7d1)",
+          },
+        },
+      },
+      animation: {
+        scale: "scale 5s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
